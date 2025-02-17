20e6883123a5a7244b312a26d699e4dee3973957
mnusrullah104 <mnusrullah104@gmail.com>
2025-02-18 00:23:16 +0500

    Your commit message

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,9 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   images: {
-//     domains: ["cdn.sanity.io"], // Allow images from Sanity's CDN
-//   },
-// };
-
-// module.exports = nextConfig;
-  
-
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     domains: ["cdn.sanity.io"], // Allow images from Sanity's CDN
   },
-  productionBrowserSourceMaps: true,
 };
 
 module.exports = nextConfig;
+
